4efda3ccb0c9238730443ce888c1912da5d43e4f
wolfBerry <906368017@@qq.com>
2020-09-08 19:35:10 +0800

    feat：reduce实现promise顺序执行

---
+++ codes/js异步/xhr.js
@@ -0,0 +1,21 @@
+const createPromise = (time, id) => () =>
+    new Promise((solve) =>
+        setTimeout(() => {
+            console.log("promise", id)
+            solve()
+        }, time)
+    )
+
+runPromiseByQueue([
+    createPromise(3000, 1),
+    createPromise(2000, 2),
+    createPromise(1000, 3),
+])
+
+function runPromiseByQueue(myPromises) {
+    myPromises.reduce(
+        (previousPromise, nextPromise) =>
+            previousPromise.then(() => nextPromise()),
+        Promise.resolve()
+    )
+}
